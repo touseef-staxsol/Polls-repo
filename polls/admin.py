3ffb4c35bf0c98311921dc53d92000f52b843ff7
touseef-staxsol <tahmad@staxsol.com>
2022-03-22 19:04:06 +0500

    Admin change and statics files

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,15 @@ from random import choices
 from django.contrib import admin
 from .models import Question,Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['question_text']
+
+class ChoicesAdmin(admin.ModelAdmin):
+    fields = ['question','choice_text','votes']
+
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice,ChoicesAdmin)
+
+
+
