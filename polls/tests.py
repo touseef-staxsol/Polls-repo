ffb04a6bce8465cdc32e56e78a2995b5b96166ca
touseef-staxsol <tahmad@staxsol.com>
2022-03-22 18:12:15 +0500

    Tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+from .models import Question
 
-# Create your tests here.
+class QuestionModelTests(TestCase):
+    
+    def test_published_with_recent(self):
+    
+        time = timezone.now() - datetime.timedelta(hours=13, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
