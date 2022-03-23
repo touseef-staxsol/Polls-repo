638e42e558f99559c007fea07f650ce0c45fb95e
touseef-staxsol <tahmad@staxsol.com>
2022-03-23 15:56:24 +0500

    added back

---
+++ polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views 
+
+app_name='polls'
+
+urlpatterns = [
+    path('',views.index,name="index"),
+    path('<int:question_id>/detail/',views.detail,name="detail"),
+    path('<int:question_id>/result/',views.results,name="result"),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
